de04df497f4d00e1142863275e59cba21701aa40
jfengsky <jfengsky@gmail.com>
2012-04-20 23:27:25 +0800

    add UA

---
+++ UA/ua.js
@@ -0,0 +1,14 @@
+﻿/**
+*	判断浏览器是否ie
+*	是则返回ie版本
+*	不是则返回false
+*/
+var UA = {
+	ie:function(){
+		if(!window.ActiveXObject){
+			return false;
+		}else if(document.all){
+			return parseInt(((navigator.userAgent.toLowerCase()).match(/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [0,'0'])[1]);	//参考了jquery的方法
+		}
+	}
+}
