fdc57703ea0522390f501798cd4e7404177cbe91
jfengsky <jfengsky@gmail.com>
2012-04-25 23:39:14 +0800

    siz

---
+++ siz/siz.js
@@ -0,0 +1,29 @@
+﻿/**
+*	id选择器和标签选择器
+*	
+*	
+*/
+function $(id){
+	return document.getElementById(id);
+}
+
+/* 下面是prototype的写法 */
+/*
+function $() { 
+	var elements = new Array(); 
+	for (var i = 0; i < arguments.length; i++){ 
+		var element = arguments[i]; 
+		if (typeof element == 'string') 
+			element = document.getElementById(element); 
+		if (arguments.length == 1) 
+			return element; 
+		elements.push(element); 
+	} 
+	return elements; 
+}
+*/
+
+function $E(id,el){
+	var elements = document.getElementById(id);
+	return elements.getElementsByTagName(el);
+}
