90e1a74bc6e75f557c8d12fa35ee194f54549d1c
jfengsky <jfengsky@gmail.com>
2012-05-07 16:24:39 +0800

    modify ua

--- UA/ua.js
+++ UA/ua.js
@@ -1,8 +1,10 @@
 ﻿/**
-*	判断浏览器是否ie
-*	是则返回ie版本
-*	不是则返回false
-*/
+ * User: jiangfeng
+ * Date: 12-5-7
+ * Time: 16:12
+ *  判断浏览器是否ie,是则返回ie版本,不是则返回false
+ * document.all是IE浏览器才有的,可以直接用来判断是否IE
+ */
 var UA = {
 	ie:function(){
 		if(!window.ActiveXObject){
