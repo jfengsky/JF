000a3225f2231499a1baec1a1b7311a1a5fa10a5
jfengsky <jfengsky@gmail.com>
2012-05-18 19:54:16 +0800

    modify ajax.js

--- build/ajax/ajax.js
+++ build/ajax/ajax.js
@@ -4,13 +4,14 @@
  * @time 12-5-17 下午5:27
  * @param url string 请求地址
  * @param type string 请求格式 POST或GET
- * @param param object 发送参数
+ * @param param object 发送参数 json格式
  * @param fn object 执行函数
+ * @param datatype string 返回数据格式，目前只支持text json
  * @return
  * 自己封装的ajax
  */
 
-function ajax(type,url,param,fn){
+function ajax(type,url,param,fn,datatype){
     var xmlHttp;
     if(window.ActiveXobject){
         xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
@@ -28,7 +29,11 @@ function ajax(type,url,param,fn){
     xmlHttp.onreadystatechange = function(){
         if(xmlHttp.readyState == 4){
             if(xmlHttp.status == 200){
-                fn(xmlHttp.responseText)
+                if( datatype == 'json'){
+                    fn(eval("(" + xmlHttp.responseText + ")"));
+                }else if(datatype == 'text'){
+                    fn(xmlHttp.responseText)
+                }
             }
         }else{
             //console.log('error');
