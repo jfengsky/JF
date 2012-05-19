f53e7a8cc4d92b6ba13656fb49889932710265eb
jfengsky <jfengsky@gmail.com>
2012-05-19 08:37:39 +0800

    添加 event模块

---
+++ demo/event/keypress.js
@@ -0,0 +1,12 @@
+﻿/**
+ * 键盘按键
+ * @author jiangfeng
+ * @time 12-5-18 22:45
+ * @param id string
+ * @param name string 属性名
+ * @return 属性值
+ */
+function getAttr(id,name){
+    var Key = {};
+}
+
---
+++ demo/event/mousewhell.js
@@ -0,0 +1,12 @@
+﻿/**
+ * 鼠标滚轮事件
+ * @author jiangfeng
+ * @time 12-5-18 22:45
+ * @param id string
+ * @param name string 属性名
+ * @return 属性值
+ */
+function getAttr(id,name){
+    var Key = {};
+}
+
