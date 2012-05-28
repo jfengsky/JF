008dc2059fc402a24a52ff958bea989dd17ac908
jfengsky <jfengsky@gmail.com>
2012-05-28 18:33:23 +0800

    修改 anim

--- demo/anim/tween.js
+++ demo/anim/tween.js
@@ -15,5 +15,18 @@
 function Tween(){
     this.linear = function(tagTime, startPosition, endPosition, needTime){
         return tagTime * endPosition / needTime + startPosition;
+    };
+    this.easeIn = function(t, b, c, d){
+        return c*(t/=d)*t + b;
+    };
+    this.easeOut = function(t, b, c, d){
+        return -c *(t/=d)*(t-2) + b;
+    };
+    this.easeInOut = function(t,b,c,d){
+        if ((t/=d/2) < 1) return c/2*t*t + b;
+        return -c/2 * ((--t)*(t-2) - 1) + b;
+    };
+    this.queeaseOut = function(t,b,c,d){
+        return -c * ((t=t/d-1)*t*t*t - 1) + b;
     }
 }
