069ad6abdbfb70470830d42141cb672794a3d67d
jfengsky <jfengsky@gmail.com>
2013-09-09 09:18:57 +0800

    add v0.2

---
+++ v0.2/demo/JF.js
@@ -0,0 +1,6 @@
+/**
+ * User: jfengsky@gmail.com
+ * Date: 13-9-9
+ * Time: 上午9:18
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ v0.2/demo/load.js
@@ -0,0 +1,6 @@
+/**
+ * User: jfengsky@gmail.com
+ * Date: 13-9-9
+ * Time: 上午9:17
+ * To change this template use File | Settings | File Templates.
+ */
